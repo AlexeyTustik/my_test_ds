3301a83ba59e35ef643bc67ec7b17e2d81ae5dae
Alexey Tustik <alexeytystik@gmail.com>
2022-07-03 14:11:40 +0000

    add model pipeline

--- ReviewAPI/api/apps.py
+++ ReviewAPI/api/apps.py
@@ -2,7 +2,28 @@ import os
 import joblib
 from django.apps import AppConfig
 from django.conf import settings
+from sklearn.base import BaseEstimator, TransformerMixin
+from nltk.stem.porter import PorterStemmer
+import re
+
+class TextTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        self.stemmer = PorterStemmer()
+        return self
+
+    def formatting(self, text):
+        words = re.sub('[^a-zA-Z]', ' ', text).lower().split()
+        words_stemmed = [self.stemmer.stem[word] for word in words]
+        return ' '.join(words_stemmed)
+
+    def transform(self, X):
+        X_copy = X.copy()
+        return X_copy.apply(self.formatting)
+
 
 class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    transformer_file = os.path.join(settings.MODELS, 'transformer.joblib')
+    model_file = os.path.join(settings.MODELS, 'final_pipeline.joblib')
+    TextTransformer = joblib.load(transformer_file)
+    model = joblib.load(model_file)
--- ReviewAPI/api/views.py
+++ ReviewAPI/api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from .apps import ApiConfig
+from .apps import ApiConfig, TextTransformer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 class ReviewClassification(APIView):
 	def post(self, request):
 		data = request.data
-		return Response(data, status=200)
+        text = data['text']
+        model = ApiConfig.model
+        result = model.predict([text])
+		return Response(result, status=200)
