5c619b3e2af33dd8fba5929401b5f1a3bdb6853d
Alexey Tustik <alexeytystik@gmail.com>
2022-07-03 09:48:54 +0000

    create Empty POST

--- ReviewAPI/api/views.py
+++ ReviewAPI/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .apps import ApiConfig
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class ReviewClassification(APIView):
+	def post(self, request):
+		data = request.data
+		return Response(data, status=200)
