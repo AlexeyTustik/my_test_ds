1a465e93627f4765ce2bdce26f66b480bca39806
Alexey <62009679+AlexeyTustik@users.noreply.github.com>
2022-07-03 22:19:05 +0300

    remove DS_Store + validate api

--- test_project/api/views.py
+++ test_project/api/views.py
@@ -5,9 +5,14 @@ from rest_framework.response import Response
 
 class ReviewClassification(APIView):
     def post(self, request):
+        if not 'text' in data:
+            return Response('no text in payload', status=400)
         data = request.data
         text = data['text']
         model = ApiConfig.model
         text_stemmed = ApiConfig.stem_text(text)
+        if text_stemmed == '':
+            return Response('no words in text', status=400)
         result = model.predict([text])
-        return Response(result, status=200)
+        result_dict = {'score': result, 'type' : 'positive' if result > 5 else 'negative'}
+        return Response(result_dict, status=200)
